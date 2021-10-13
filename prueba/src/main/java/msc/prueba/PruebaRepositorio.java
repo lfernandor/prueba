cd098736f78c35fc1a29efbb8bbfe9ccf6ffc1e1
lfernandor <84603918+lfernandor@users.noreply.github.com>
2021-10-13 00:11:09 -0500

    hola mundo 3

--- prueba/src/main/java/msc/prueba/PruebaRepositorio.java
+++ prueba/src/main/java/msc/prueba/PruebaRepositorio.java
@@ -16,7 +16,7 @@ public class PruebaRepositorio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         System.out.println("Hola ");
+         System.out.println("Hola Mundo 3");
     }
     
 }
